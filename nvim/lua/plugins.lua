7d33d39a5fed413ddd848acfa785d4692891a3d9
Tanishq Singla <tanishqsingla00@gmail.com>
2023-08-09 23:35:43 +0530

    Update to latest minimal config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,39 @@
 vim.cmd [[packadd packer.nvim]]
-return require('packer').startup(function(user)
-  use 'wbthomason/packer.nvim'
+
+require('packer').startup(function(user)
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+
+	use 'kyazdani42/nvim-web-devicons'
+
+  use 'folke/tokyonight.nvim'
+
+  use {'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons'
+    }
+  }
+
+  use {
+    "akinsho/toggleterm.nvim", tag = '*', config = function()
+      require("toggleterm").setup()
+    end
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+   -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use 'lewis6991/gitsigns.nvim'
 
   use 'neovim/nvim-lspconfig'
 
@@ -14,43 +47,10 @@ return require('packer').startup(function(user)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
 
-  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
-  use 'folke/tokyonight.nvim'
-
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-
-  use 'kyazdani42/nvim-web-devicons'
-
-
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-  }
-
-  use {
-    "akinsho/toggleterm.nvim", tag = '*', config = function()
-      require("toggleterm").setup()
-    end
-  }
-
   use {
     "williamboman/mason.nvim",
     run = ":MasonUpdate" -- :MasonUpdate updates registry contents
   }
 
-
   use "williamboman/mason-lspconfig.nvim"
-
 end)
